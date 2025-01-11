db4df62de20b38c5bcffd945bece8d2c046a2372
nishaaant <nishaaant1608@gmail.com>
2025-01-11 15:33:45 +0530

    minor changes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const cookieParser = require("cookie-parser")
 
 
 //importing route
@@ -18,6 +19,7 @@ app.use(
   );
 //parsing json data here - data will first enter here
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/", authRouter)
 app.use("/",router);
--- routes/mailRouter.js
+++ routes/mailRouter.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const { mailController } = require("../controllers/mailController");
 const { validation } = require("../middlewares/validation");
+const {authUser} = require("../utils/auth")
 
 const router = express.Router();
 
-router.post("/sendemail" , validation , mailController);
+router.post("/sendemail" ,authUser, validation , mailController);
 module.exports = {router};
